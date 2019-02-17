b810dd032b3fa9ae006ae88583e278fc346cf088
kyle <cookekyle97@gmail.com>
2019-02-17 10:41:51 +0000

    rename control page to index

--- source/javascripts/chrome.js
+++ source/javascripts/chrome.js
@@ -2,7 +2,7 @@
 // manifest.json)
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('control.html', {
+  chrome.app.window.create('index.html', {
     type: 'panel'
   });
 });
